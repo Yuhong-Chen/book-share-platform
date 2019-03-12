33841a866c4793cc52ae9eaf7835a07d5ef9af96
VictorIJnr <victorijnr@gmail.com>
2019-03-12 23:30:25 +0000

    Added a blank Splash screen.
    This screen will ultimately form the entrypoint for the system in the
    java GUI.

--- src/impl/GUI/Splash.java
+++ src/impl/GUI/Splash.java
@@ -1,4 +1,26 @@
 package impl.GUI;
 
-public class Splash {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Splash extends Application {
+
+    @Override
+    public void start(Stage myStage) {
+        VBox mainBox = new VBox();
+
+        Scene splashScene = new Scene(mainBox, 720, 480);
+        splashScene.getStylesheets().addAll(Splash.class.getResource("/css/Sitewide.css").toExternalForm());
+
+        myStage.setScene(splashScene);
+        myStage.setTitle("Welcome to BookyBooks!");
+        myStage.show();
+    }
+
+    public static void main(String[] myArgs) {
+        launch(myArgs);
+    }
+
 }
